768897e8f16f965e4601ba7efe1cc5ec45892643
yahya dalbah <me@yahyadalbah.com>
2023-02-25 18:57:57 +0200

    modified

--- src/App.js
+++ src/App.js
@@ -1,47 +1,69 @@
-import React from "react";
+import React, { useEffect, useMemo, useRef } from "react";
 import Question from "./question";
 import {nanoid} from "nanoid"
 import { useState } from "react";
 import he from "he"
 
 export default function App(){
-    const [questionsArray,setQuestionsArray] = React.useState([])
+    const [questionsArray,setQuestionsArray] = useState([])
+    const [holdedanswers, sethold] = useState([])
     function getquestions(){
-        fetch("https://opentdb.com/api.php?amount=5&type=multiple").then(res => res.json()).then(data => setQuestionsArray(data.results))
+        fetch("https://opentdb.com/api.php?amount=5&type=multiple").then(res => res.json()).then(data => data.results).then(questions => {
+            const arr = questions.map(q => {
+                let answers = shuffle([...q.incorrect_answers, q.correct_answer])
+                return {
+                    ...q,
+                    answers
+                }
+            })
+            return setQuestionsArray(arr)
+        })
+        sethold([])
     }
+    
     const [start,setstart] = React.useState(true)
-    const [check,setcheck] = React.useState(false)
-    const numOfCorrectAnswers = React.useRef(0)
+    const [check, setcheck] = useState(false)
+    const numOfCorrectAnswers = useRef(0)
 
-    
-        
-    
+    function shuffle(array) {
+        let currentIndex = array.length,  randomIndex;
 
-    const questionObj = React.useMemo(()=>questionsArray.map((q,i) => {
-        
-        return <Question 
-        key={nanoid()}
-        id={i}
-        question={he.decode(q.question)}
-        incorrectanswers={(q.incorrect_answers.map(inc => he.decode(inc)))}
-        correctanswer= {he.decode(q.correct_answer)}
-        getquestions={getquestions}
-    />}),[check,questionsArray])
-    
-    
+        while (currentIndex != 0) {
+
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+            [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex], array[currentIndex]];
+        }
+
+        return array;
+    }
+
+    const questionObj = questionsArray.map((q,i) => 
+        <Question 
+            key={nanoid()}  
+            id={i}
+            question={he.decode(q.question)}
+            answers={(q.answers.map(inc => he.decode(inc)))}
+            correctanswer={he.decode(q.correct_answer)}
+            checkanswers = {checkanswers}
+            check = {check}
+            holdedanswers = {holdedanswers}
+        />
+    )
     function checkanswers(answer, correctanswer){
-        
         if(answer && answer === correctanswer){
            numOfCorrectAnswers.current = numOfCorrectAnswers.current + 1
         }
         
-
     }
     function showresult(){
         setcheck(prev => !prev)
         if(check){
             numOfCorrectAnswers.current = 0
         }
+        
     }
     
     
@@ -59,12 +81,19 @@ export default function App(){
                 <button onClick={startquiz} className="btn">Start quiz</button>
             </div> : <section>
                     {questionObj} 
+                    <div className="checkedDiv">
+                        {!check ? <button onClick={() => {
+                            checkanswers()
+                            showresult()
+                        }} className="btn check-btn play-again-btn">check answers</button> : <button onClick={() => {
+                            getquestions(); 
+                            checkanswers()
+                            showresult()
+                        }} className="btn check-btn play-again-btn">play again</button>}
+                        
+                        {check && <h1>you scored {numOfCorrectAnswers.current}/5</h1>}
+                    </div>
                     
-                    <button onClick={() => {
-                        getquestions(); 
-                        checkanswers()
-                        showresult()
-                    }} className="btn check-btn play-again-btn">{"play again"}</button>
                     
                 </section>}
         </main>
--- src/question.js
+++ src/question.js
@@ -1,65 +1,34 @@
-import React, {useRef, useState} from "react";
+import React, {useState} from "react";
 
-export default function Question({question,incorrectanswers,correctanswer,getquestions}){
-    const answers = [...incorrectanswers,correctanswer]
-    const [shuffledanswers,setshuffledanswers] = useState(shuffle(answers))
-    const [holdedanswer,setholdedanswer] = useState(null)
+export default function Question({question,answers,correctanswer, checkanswers, check, holdedanswers}){
+    const [holdedanswer, setHoldedanswer] = useState(null)
     const haspickedanswer = holdedanswer !== null
-    const [check,setcheck] = React.useState(false)
-    const numOfCorrectAnswers = React.useRef(0)
+    
+    console.log(holdedanswers)
 
-    function checkanswers(answer, correctanswer){
-        
-        if(answer && answer === correctanswer){
-           numOfCorrectAnswers.current = numOfCorrectAnswers.current + 1
-        }
-        
-
-    }
-    function showresult(){
-        setcheck(prev => !prev)
-        if(check){
-            numOfCorrectAnswers.current = 0
-        }
-    }
-
-    function shuffle(array) {
-        let currentIndex = array.length,  randomIndex;
-
-        while (currentIndex != 0) {
-
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
-
-            [array[currentIndex], array[randomIndex]] = [
-                array[randomIndex], array[currentIndex]];
-        }
-
-        return array;
-    }
-
-    const displayedAnswers = shuffledanswers.map((answer,i) => {
+    const displayedAnswers = answers.map((answer,i) => {
         let className = "answer-btn"
         
         if(check && answer === correctanswer){
             className += " correct-answer"
         }
-        if(answer === holdedanswer){
+        if(holdedanswers.includes(answer)){
             className += " picked"
             if(check && answer !== correctanswer){
-                className +=" incorrect-answer"
+                className += " incorrect-answer"
             }
         }
         return <button 
         key={i}
         disabled={check || haspickedanswer}
-        onClick={holdanswer}
+        onClick={e => holdanswer(e)}
         className={className}>{answer}</button>}
     )
 
     function holdanswer(e){
         const playerAnswer = e.target.innerHTML
-        setholdedanswer(playerAnswer)
+        setHoldedanswer(playerAnswer)
+        holdedanswers.push(playerAnswer)
         checkanswers(playerAnswer,correctanswer)
     }
 
@@ -71,13 +40,7 @@ export default function Question({question,incorrectanswers,correctanswer,getque
         <div className="form">
             {displayedAnswers}
         </div>
-        
-        <button onClick={() => {
-                         
-                        checkanswers()
-                        showresult()
-                    }} disabled={check} className="btn check-btn">{"check answer"}</button>
-                    <h1 className="play-again-h1">{check && `u scored ${numOfCorrectAnswers.current}/1`}</h1>
+        <br />
        <hr />
        </div>
     )
